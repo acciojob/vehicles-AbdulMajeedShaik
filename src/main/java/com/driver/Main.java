317f7cb8b3ce50459668b02e7c77385bc4c0dcaa
AbdulMajeedShaik <shaika854@gmail.com>
2023-10-14 10:39:51 +0530

    Task Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,9 +2,13 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        F1 mercedes = new F1("mercedes", false);
+        F1 mercedes = new F1("Mercedes F1", false);
         mercedes.move(40, 0);
         mercedes.accelerate(20);
         mercedes.accelerate(-60);
+
+        Boat boat = new Boat("SpeedBoat", 6);
+        System.out.println("Boat Name: " + boat.getVehicleName());
+        System.out.println("Boat Capacity: " + boat.getVehicleCapacity());
     }
 }
